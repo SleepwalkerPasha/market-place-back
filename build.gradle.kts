2b4aaea4af974357e73ea9f2ba397358388909c3
Pavel Egorov <sleepwalkerpasha@ya.ru>
2023-09-16 17:04:24 +0300

    add detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,8 @@ plugins {
     id("org.springframework.boot") version "3.1.3"
     id("io.spring.dependency-management") version "1.1.3"
     kotlin("jvm") version "1.8.22"
-    kotlin("plugin.spring") version "1.8.22"
+    kotlin("plugin.spring") version "1.9.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.1"
 }
 
 group = "ru.sharpbang"
