f8df423fddc987ff90d9fdb0d7e2f220da9e02b9
mikarpechenkov <mikarpechenkov@gmail.com>
2023-09-18 03:45:50 +0300

    add restClient for restService and add endpoint for get product

--- common-utils/build.gradle.kts
+++ common-utils/build.gradle.kts
@@ -3,3 +3,6 @@ plugins {
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.9.0"
 }
+dependencies{
+    implementation("jakarta.validation:jakarta.validation-api:3.0.2")
+}
--- restservice/src/main/kotlin/ru/sharpbang/marketplaceback/restservice/model/response/ProductResponse.kt
+++ restservice/src/main/kotlin/ru/sharpbang/marketplaceback/restservice/model/response/ProductResponse.kt
@@ -1,11 +1,11 @@
 package ru.sharpbang.marketplaceback.restservice.model.response
 
-import ru.sharpbang.marketplaceback.restservice.model.request.ImageRequest
 import java.math.BigDecimal
 
-data class ProductResponse (
-    val title:String,
-    val description:String?=null,
+data class ProductResponse(
+    val id: Long,
+    val title: String,
+    val description: String?,
     val price: BigDecimal,
-    var pictures: Set<ImageRequest>?=null
+    val picturesIds: Set<Long>?
 )
